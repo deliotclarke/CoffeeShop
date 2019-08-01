975e4017ba3b5f79dbe8058dcc825029f750de2c
deliotclarke <deliotclarke@gmail.com>
2019-08-01 12:33:14 -0500

    added more annotations to Coffee model.

--- CoffeeShop/Models/Coffee.cs
+++ CoffeeShop/Models/Coffee.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace CoffeeShop.Models
@@ -11,15 +12,18 @@ namespace CoffeeShop.Models
         public int Id { get; set; }
 
         // if you're trying to post and you don't have a title on the object this will deny it
+        // these are only for data being posted up
         [Required]
         [MaxLength(50)]
         public string Title { get; set; }
 
         // setting min max on a property
-        //can also set two
+        // can also set two
+        // and also create a custom error message
         [Required]
-        [MaxLength(50)]
+        [MaxLength(50, ErrorMessage = "BeanType way too loooooong")]
+        // ignore data member will ignore this prop during serialization - it won't show up in the JSON you're getting back in a request
+        [IgnoreDataMember]
         public string BeanType { get; set; }
-
     }
 }
